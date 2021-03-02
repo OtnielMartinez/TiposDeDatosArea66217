70bc2275b3c9c108967258f02a80f91204e0d1c0
lampi <lampi@192.168.1.78>
2021-03-02 07:33:53 -0600

    Commit para repocitorio

---
+++ src/tiposdedatosarea66217/TiposDeDatosArea66217.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatosarea66217;
+
+/**
+ *
+ * @author lampi
+ */
+public class TiposDeDatosArea66217 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Calcular el area de un rectangulo con base 8 y altura 15
+        int alturadelrectangulo =15;
+        int basedelrectangulo = 8;
+        int areadelrectangulo = alturadelrectangulo * basedelrectangulo;
+        
+        System. out. println("El area del rectangulo es  = " + areadelrectangulo);
+            
+        
+    }
+    
+}
